e9ff1f2a004057db4b527da373e56c2bd4e8ab1f
Aniket Gavali <aniket.gavali@noovosoft.com>
2025-08-08 16:28:56 +0530

    logger

--- app.py
+++ app.py
@@ -8,7 +8,20 @@ def root_status():
 
 
 import logging
+
 logger = logging.getLogger("app")
+logger.setLevel(logging.INFO)
+
+# Console handler (stdout)
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+# Formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+
+# Add handler to logger
+logger.addHandler(console_handler)
 @app.route("/doku/payment-notification", methods=["POST"])
 def doku_notification():
     data = request.get_json(force=True)  # Parse JSON body
